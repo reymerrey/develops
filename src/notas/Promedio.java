a31c44d2e6141d4f9a0474abfa8167182a021902
rcaunahu <rcaunahu@192.168.1.6>
2025-06-19 18:37:28 -0500

    primero cambio

---
+++ src/notas/Promedio.java
@@ -0,0 +1,23 @@
+package notas;
+
+import java.util.Scanner;
+
+public class Promedio {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+        int mejorNota = 0;
+
+        System.out.println("Introduce las 4 notas del alumno:");
+        for (int i = 0; i < 4; i++) {
+            System.out.print("Nota " + (i + 1) + ": ");
+            int nota = scanner.nextInt();
+            if (nota > mejorNota) {
+                mejorNota = nota;
+            }
+        }
+
+        System.out.println("La mejor nota es: " + mejorNota);
+        scanner.close();
+	}
+
+}
